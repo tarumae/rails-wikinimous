d4adae573d9ad5d2baaa48e47f9f03f34ef60bce
Toomas Arumae <tom.arumae@gmail.com>
2021-02-16 16:05:20 +0000

    Create all 7 CRUD actions and style the website a bit. Can not get font awesome to work even with fa_icon helper and fa-rails installed. Must investigate õ_õ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+  new_article = Article.new(title: Faker::Beer.brand, content: Faker::Beer.name)
+  new_article.save
+end
